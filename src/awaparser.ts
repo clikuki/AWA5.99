a54c5bb47e0eeeefb6937c714e3f5a99fe1949c6
clikuki <lanzvasquez24@gmail.com>
2026-08-23 20:30:12 +0800

    awaparser module added

---
+++ src/awaparser.ts
@@ -0,0 +1,48 @@
+function
+parseAwas(awaStr: string): boolean[]
+{
+    const bitArray: boolean[] = [];
+    const matchAgainst = "AWA";
+
+    let matchIdx = 0,
+        foundChecksumAwa = false,
+        inAwaSequence = false,
+        i = 0,
+        char: string;
+
+    for(; i < awaStr.length; i++)
+    {
+        char = awaStr[i].toUpperCase();
+
+        if(char === " ")
+        {
+            matchIdx = 0;
+            inAwaSequence = false;
+        }
+
+        if(char !== matchAgainst[matchIdx])
+        {
+            matchIdx = 0;
+        }
+        else if(++matchIdx > 2)
+        {
+            matchIdx = 1;
+
+            if(inAwaSequence)
+            {
+                bitArray.push(true);
+            }
+            else if(foundChecksumAwa)
+            {
+                inAwaSequence = true;
+                bitArray.push(false);
+            }
+            else
+            {
+                foundChecksumAwa = true;
+            }
+        }
+    }
+
+    return bitArray;
+}
